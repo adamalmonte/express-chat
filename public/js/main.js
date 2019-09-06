f457f36cf87cb5b9e3130082e465977fa3ad5406
Adam Almonte <adamgalmonte@gmail.com>
2019-09-05 23:40:28 -0400

    User submission working. Styling still WIP

---
+++ public/js/main.js
@@ -0,0 +1,73 @@
+const form = document.getElementById('js-message-composer');
+form.addEventListener('submit', appendTimestampAndPOST);
+
+const messages = document.getElementsByClassName('js-post-message');
+for (var i = 0; i < messages.length; i++) {
+    messages[i].addEventListener('click', handleMessageClick);
+}
+
+function appendTimestampAndPOST(ev) {
+    ev.preventDefault();
+
+    const messageField = form.querySelector('.js-message');
+    const timestampField = form.querySelector('.js-timestamp');
+    timestampField.value = Math.floor(new Date().getTime() / 1000);
+
+    const formBody = {
+        'message': messageField.value,
+        'ts': timestampField.value
+    };
+
+    fetch('/chat/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formBody)
+    })
+    .then(res => res.json())
+    .then(response => {
+        console.log('Success:', JSON.stringify(response));
+        appendMessage(response);
+        messageField.value = '';
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function appendMessage(data) {
+    const postsContainer = document.getElementById('js-posts');
+
+    const newMessage = document.createElement('div');
+    newMessage.className = "post post--user";
+
+    const postHTML = `
+        <div class="post__image-wrapper">
+            <img class="post__image" src="/images/better.jpg">
+        </div>
+        <div class="post__content">
+            <div class="post__header">
+                <div class="post__timestamp">
+                    <span class="post__time-from-now">${data.tsFromNow}</span>
+                </div>
+            </div>
+            <div class="post__message  js-post-message">
+                <div class="post__message-front">
+                    ${data.message}
+                </div>
+                <div class="post__message-back">
+                    <span><i class="material-icons">calendar_today</i> active since ${data.tsFormatted}</span> 
+                </div> 
+            </div>
+        </div>            
+    `;
+
+    newMessage.innerHTML = postHTML;
+    newMessage.onclick = handleMessageClick;
+
+    postsContainer.appendChild(newMessage);
+}
+
+function handleMessageClick(ev) {
+    const el = ev.target;
+    el.classList.toggle('post__message--active');
+}
--- routes/chat.js
+++ routes/chat.js
@@ -1,15 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var getChat = require('./../data/data');
+var moment = require('moment');
 
 var state = getChat();
 
 router.route('/')
     .get((req, res) => {
+        // return state
         res.json(state);
     })
     .post((req, res) => {
-        //accept a single new post
+        // accept a single new post
+
+        const message = req.body.message;
+        const ts = req.body.ts;
+        const tsFromNow = moment.unix(ts).fromNow();
+        const tsFormatted = moment.unix(ts).format("MMM D, Y");
+
+        const newMessage = {
+            "message": message,
+            "ts": ts
+        };
+
+        const response = {
+            "message": message,
+            "ts": ts,
+            "tsFromNow": tsFromNow,
+            "tsFormatted": tsFormatted
+        };
+
+        state.posts.push(newMessage);
+
+        res.json(response);
     });
 
 module.exports = router;
