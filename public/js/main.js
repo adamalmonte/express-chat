d2298f316aea8e093ce7df85fd1a2a7b31b552f4
Adam Almonte <adamgalmonte@gmail.com>
2019-09-06 00:51:37 -0400

    Character count basic implementation in. client-side JS organization and mobile need work

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,23 @@
 const form = document.getElementById('js-message-composer');
 form.addEventListener('submit', appendTimestampAndPOST);
 
+const messageField = form.querySelector('.js-message');
+const maxChars = 140;
+
 const messages = document.getElementsByClassName('js-post-message');
-for (var i = 0; i < messages.length; i++) {
-    messages[i].addEventListener('click', handleMessageClick);
+for (let i = 0; i < messages.length; i++) {
+    messages[i].addEventListener('touchstart', toggleMessageActiveState);
+    messages[i].addEventListener('mouseover', toggleMessageActiveState);
+    messages[i].addEventListener('mouseout', toggleMessageActiveState);
 }
 
 function appendTimestampAndPOST(ev) {
     ev.preventDefault();
 
-    const messageField = form.querySelector('.js-message');
+    if (messageField.value.length > maxChars) {
+        return false;
+    }
+
     const timestampField = form.querySelector('.js-timestamp');
     timestampField.value = Math.floor(new Date().getTime() / 1000);
 
@@ -62,12 +70,43 @@ function appendMessage(data) {
     `;
 
     newMessage.innerHTML = postHTML;
-    newMessage.onclick = handleMessageClick;
+    newMessage.ontouchstart = toggleMessageActiveState;
+    newMessage.onmouseover = toggleMessageActiveState;
+    newMessage.onmouseout = toggleMessageActiveState;
 
     postsContainer.appendChild(newMessage);
 }
 
-function handleMessageClick(ev) {
+function toggleMessageActiveState(ev) {
     const el = ev.target;
     el.classList.toggle('post__message--active');
 }
+
+function characterCounter() {
+    const counterEl = document.getElementById('js-character-count');
+
+    const errorClass = 'message-composer__form--error';
+    const warningClass = 'message-composer__form--warning';
+
+    messageField.onkeydown = function() {
+        setTimeout(function(){
+            let remaining = maxChars - messageField.value.length;
+            counterEl.innerHTML = remaining;
+            
+            if (remaining < 0) {
+                form.classList.add(errorClass);
+            } else if (remaining <= 20) {
+                form.classList.add(warningClass);
+            } else {
+                if (form.classList.contains(errorClass)){
+                    form.classList.remove(errorClass)
+                }
+                if (form.classList.contains(warningClass)){
+                    form.classList.remove(warningClass)
+                }
+            }
+        }, 1);
+    }
+}
+
+characterCounter();
